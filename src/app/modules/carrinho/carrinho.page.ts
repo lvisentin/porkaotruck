4dfde0b5ee43b90b8c463d0d08c25d9e281370b5
lvise <lucas.batista@mail.tantto.com.br>
2020-08-09 13:45:10 -0300

    Layout carrinho

--- src/app/modules/carrinho/carrinho.page.ts
+++ src/app/modules/carrinho/carrinho.page.ts
@@ -9,6 +9,35 @@ export class CarrinhoPage implements OnInit {
 
   constructor() { }
 
+  public carrinho = {
+    itens: [
+      {
+        id: 1,
+        nome: 'Porkão costela',
+        descricao: 'teste',
+        valor: 24.90,
+        qtd: 3
+      },
+      {
+        id: 2,
+        nome: 'Porkão costela',
+        descricao: 'teste',
+        valor: 24.90,
+        qtd: 2
+      },
+      {
+        id: 3,
+        nome: 'Porkão costela',
+        descricao: 'teste',
+        valor: 24.90,
+        qtd: 1
+      }
+    ],
+    subtotal: 24.90,
+    taxaEntrega: 5,
+    total: 29.9
+  }
+
   ngOnInit() {
   }
 

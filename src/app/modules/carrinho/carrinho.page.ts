5d55eb05f7f0d2a558ccb35875ef5713eb823ddd
Gabriel Jorge <gabriel.jorge.pereira@outlook.com>
2020-11-29 14:16:53 -0300

    só pra pull

--- src/app/modules/carrinho/carrinho.page.ts
+++ src/app/modules/carrinho/carrinho.page.ts
@@ -8,174 +8,171 @@ import { CarrinhoService } from 'src/app/services/carrinho.service';
 import { PedidosService } from 'src/app/services/pedidos.service';
 
 @Component({
-  selector: 'app-carrinho',
-  templateUrl: './carrinho.page.html',
-  styleUrls: ['./carrinho.page.scss'],
+	selector: 'app-carrinho',
+	templateUrl: './carrinho.page.html',
+	styleUrls: ['./carrinho.page.scss'],
 })
 export class CarrinhoPage {
 
-  public carrinho = this.appcomponent.carrinho;
-  // public carrinho = carrinho;
-  private userEndereco;
-  public metodoPagamento: number;
-  public metodosPgto;
-  public entregaMin;
-  public entregaMax;
-  public user;
-  public endereco;
+	public carrinho = this.appcomponent.carrinho;
+	// public carrinho = carrinho;
+	private userEndereco;
+	public metodoPagamento: number;
+	public metodosPgto;
+	public entregaMin;
+	public entregaMax;
+	public user;
+	public endereco;
 
-  constructor(
-    private appcomponent: AppComponent,
-    private popoverController: PopoverController,
-    private router: Router,
-    private carrinhoService: CarrinhoService,
-    private pedidosService: PedidosService,
-    private readonly loadingController: LoadingController,
-    private readonly alertController: AlertController,
-  ) { }
+	constructor(
+	private appcomponent: AppComponent,
+	private popoverController: PopoverController,
+	private router: Router,
+	private carrinhoService: CarrinhoService,
+	private pedidosService: PedidosService,
+	private readonly loadingController: LoadingController,
+	private readonly alertController: AlertController,
+	) { }
 
 
-  ionViewDidEnter() {
-    console.log(this.carrinho)
-    this.user = JSON.parse(localStorage.getItem("user"));
+	ionViewDidEnter() {
+	console.log(this.carrinho);
+	this.user = JSON.parse(localStorage.getItem('user'));
 
-    const taxa = JSON.parse(localStorage.getItem('taxaEntrega'))
+	const taxa = JSON.parse(localStorage.getItem('taxaEntrega'));
 
-    carrinho.setTaxaEntrega(taxa.vlpreco);
+	carrinho.setTaxaEntrega(taxa.vlpreco);
 
-    this.entregaMax = taxa.tempo_max;
-    this.entregaMin = taxa.tempo_min;
+	this.entregaMax = taxa.tempo_max;
+	this.entregaMin = taxa.tempo_min;
 
-    this.userEndereco =  JSON.parse(localStorage.getItem('userEndereco'));
+	this.userEndereco =  JSON.parse(localStorage.getItem('userEndereco'));
 
-  }
+	}
 
-  ngOnInit() {
-    this.endereco = JSON.parse(localStorage.getItem('endereco'));
-    this.carrinhoService.getMetodosPagamento()
-      .subscribe(
-        (metodosPgto) => {
-          this.metodosPgto = metodosPgto['data'];
-          console.log(this.metodosPgto)
-        }
-      )
+	ngOnInit() {
+		this.endereco = JSON.parse(localStorage.getItem('endereco'));
+		this.carrinhoService.getMetodosPagamento()
+			.subscribe(
+			(metodosPgto) => {
+				this.metodosPgto = metodosPgto['data'];
+				console.log(this.metodosPgto);
+			}
+			);
 
-    console.log(this.carrinho)
-  }
+		console.log(this.carrinho);
+	}
 
-  async presentPopoverOpt(idProd: any) {
-    console.log('ev', idProd)
-    const popover = await this.popoverController.create({
-      component: PopoverCarrinhoComponent,
-      cssClass: 'popover-carrinho',
-      // event: ev,
-      translucent: false,
-    })
+	async presentPopoverOpt(idProd: any) {
+	console.log('ev', idProd);
+	const popover = await this.popoverController.create({
+		component: PopoverCarrinhoComponent,
+		cssClass: 'popover-carrinho',
+		// event: ev,
+		translucent: false,
+	});
 
-    const { data } = await popover.onDidDismiss();
-    console.log('data', data)
+	const { data } = await popover.onDidDismiss();
+	console.log('data', data);
 
-    return await popover.present();
-  }
+	return await popover.present();
+	}
 
-  async presentAlertSuccess(ev: any = null) {
-    const alert = await this.alertController.create({
-      header: 'Pedido Concluído',
-      message: 'Seu pedido foi concluído com sucesso!',
-      buttons: ['OK']
+	async presentAlertSuccess(ev: any = null) {
+		const alert = await this.alertController.create({
+			header: 'Pedido Concluído',
+			message: 'Seu pedido foi concluído com sucesso!',
+			buttons: ['OK']
 
-    });
-    await alert.present();
-  }
- 
-  async presentAlertError(ev: any = null) {
-    const alert = await this.alertController.create({
-      header: 'Ocorreu um erro',
-      message: 'Ocorreu um erro na hora de finalizar seu pedido, tente novamente mais tarde!',
-      buttons: ['OK']
+		});
+		await alert.present();
+	}
 
-    });
-    await alert.present();
-  }
+	async presentAlertError(message = 'Ocorreu um erro na hora de finalizar seu pedido, tente novamente mais tarde!') {
+		const alert = await this.alertController.create({
+			header: 'Ocorreu um erro',
+			message,
+			buttons: ['OK']
 
-  async presentAlertConfirm(item) {
-    console.log('item', item)
-    const alert = await this.alertController.create({
-      header: 'Remover item',
-      message: `Deseja remover o item ${item.nome} do carrinho?`,
-      buttons: [{
-        text: 'Cancelar',
-        handler: () => {
-          this.alertController.dismiss()
-        }
-      },
-      {
-        text: 'Sim',
-        handler: () => {
-          this.removeItem(item);
-        }
-      }]
+		});
+		await alert.present();
+	}
 
-    });
-    await alert.present();
-  }
+	async presentAlertConfirm(item) {
+		const alert = await this.alertController.create({
+			header: 'Remover item',
+			message: `Deseja remover o item ${item.nome} do carrinho?`,
+			buttons: [{
+				text: 'Cancelar',
+				handler: () => {
+					this.alertController.dismiss();
+				}
+			},
+			{
+				text: 'Sim',
+				handler: () => {
+					this.removeItem(item);
+				}
+			}]
 
-  async presentLoading() {
-    const loading = await this.loadingController.create({
-      cssClass: 'loading-pedido',
-      message: 'Finalizando pedido...',
-    });
-    await loading.present();
+		});
+		await alert.present();
+	}
 
-    const { role, data } = await loading.onDidDismiss();
-  }
+	async presentLoading() {
+		const loading = await this.loadingController.create({
+			cssClass: 'loading-pedido',
+			message: 'Finalizando pedido...',
+		});
+		await loading.present();
 
-  removeItem(item) {
-    this.carrinho.removerItem(item);
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
-  }
+		const { role, data } = await loading.onDidDismiss();
+	}
 
-  finalizaPedido() {
-    if (!localStorage.getItem('user')) { this.router.navigate(['/login']) }
-    this.presentLoading();
+	removeItem(item) {
+		this.carrinho.removerItem(item);
+		localStorage.setItem('carrinho', JSON.stringify(carrinho));
+	}
 
-    const user = JSON.parse(localStorage.getItem('user'));
-    
-    console.log('carrinho', this.carrinho)
+	finalizaPedido() {
+		if (!localStorage.getItem('user')) { this.router.navigate(['/login']); }
+		this.presentLoading();
 
-    const pedido = {
-      "idendereco": this.userEndereco.id,
-      "idusuario": user.id,
-      "itens": this.carrinho.returnItensApi(),
-      "vltotal": this.carrinho.getVlTotal(),
-      "vlsubtotal": this.carrinho.getVlSubtotal(),
-      "vltaxa_entrega": this.carrinho.getVlTaxaEntrega(),
-      "idforma_pagamento": this.metodoPagamento
-    }
+		const user = JSON.parse(localStorage.getItem('user'));
 
-    console.log(pedido)
+		console.log('carrinho', this.carrinho);
 
-    this.pedidosService.createPedido(pedido)
-      .subscribe(
-        (pedido) => {
-          console.log('pedido', pedido)
-          this.loadingController.dismiss();
-          this.presentAlertSuccess();
+		const pedido = {
+			idendereco: this.userEndereco.id,
+			idusuario: user.id,
+			itens: this.carrinho.returnItensApi(),
+			vltotal: this.carrinho.getVlTotal(),
+			vlsubtotal: this.carrinho.getVlSubtotal(),
+			vltaxa_entrega: this.carrinho.getVlTaxaEntrega(),
+			idforma_pagamento: this.metodoPagamento
+		};
 
-          this.carrinho.limpaCarrinho();
-          localStorage.removeItem('carrinho');
+		this.pedidosService.createPedido(pedido)
+			.subscribe(
+			(pedido) => {
+				console.log('pedido', pedido);
+				this.loadingController.dismiss();
+				this.presentAlertSuccess();
 
-          this.router.navigate(['tabs/home']);
-        
-        }, (err) => {
-          console.log('err', err)
-          this.loadingController.dismiss();
-          this.presentAlertError();
-        }
-      )
-  }
+				this.carrinho.limpaCarrinho();
+				localStorage.removeItem('carrinho');
 
-  removerItem(item) {
-    this.carrinho.removerItem(item);
-  }
+				this.router.navigate(['tabs/home']);
+
+			}, (err) => {
+				console.log('err', err);
+				this.loadingController.dismiss();
+				this.presentAlertError(err.message);
+			}
+		);
+	}
+
+	removerItem(item) {
+	this.carrinho.removerItem(item);
+	}
 }
